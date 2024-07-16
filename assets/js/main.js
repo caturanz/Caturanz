74f3ad59c6998f277e5453e799d3b8073819d779
Caturanz <serviciocomunitariotsu@gmail.com>
2024-07-16 19:15:41 -0400

    Limpiando

--- assets/js/main.js
+++ assets/js/main.js
@@ -178,3 +178,41 @@
   document.addEventListener('scroll', navmenuScrollspy);
 
 })();
+
+$(document).ready(function(){
+  $('.customer-logos').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 5
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 2
+        }
+      }
+    ]
+  });
+});
