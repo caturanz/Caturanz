6e3622ef02cf6010023824322f902bae07e72341
Caturanz <serviciocomunitariotsu@gmail.com>
2024-07-18 11:50:56 -0400

    Ajuste afiliados

--- assets/js/main.js
+++ assets/js/main.js
@@ -181,23 +181,13 @@
 
 $(document).ready(function(){
   $('.customer-logos').slick({
-      slidesToShow: 6,
+      slidesToShow: 5,
       slidesToScroll: 1,
       autoplay: true,
-      autoplaySpeed: 1500,
+      autoplaySpeed: 900,
       arrows: false,
       dots: false,
       pauseOnHover: false,
-      responsive: [{
-          breakpoint: 768,
-          settings: {
-              slidesToShow: 4
-          }
-      }, {
-          breakpoint: 520,
-          settings: {
-              slidesToShow: 3
-          }
-      }]
+     
   });
 });
